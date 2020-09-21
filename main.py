3969ff4d7ef20f81ad8772691300c79f1ff58ec2
reejit <70316178+reejit@users.noreply.github.com>
2020-09-21 11:55:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
 import time
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-from pyrogram import Client
 
-select = " "
-
-docs = """Generate your Telegram String Session
-
-P -->> Pyrogram [https://docs.pyrogram.org]
-T -->> Telethon [https://docs.telethon.dev]
-"""
-
-tutor = """
-~ go-to my.telegram.org
-~ Login using your Telegram account
-~ Click on API Development Tools
-~ Create a new application, by entering the required details
-~ Check your Telegram saved messages section to copy the STRING_SESSION
-"""
 
 template = """
-UserBot support: @userbotindo
+UserBot support: @CAPTAINAMERICAUSERBOT
             
 <code>STRING_SESSION</code>: <code>{}</code>
 
 ⚠️ <b>Please be carefull to pass this value to third parties</b>"""
 
-
-print(docs)
-
-while select != ("p", "t"):
-    select = input("Enter your required client < p / t > : ").lower()
-    if select == "t":
-        print("""\nTelethon selected\nRunning script...""")
+while select != ("y", "n"):
+    select = input("Do you want to continue < y / n > : ").lower()
+    if select == "y":
+        print("""\nConnecting To CaptainAmericaServer\nRunning script...""")
         time.sleep(1)
         print(tutor)
         API_KEY = int(input("Enter API_KEY here: "))
@@ -47,21 +28,9 @@ while select != ("p", "t"):
             print("Your STRING_SESSION value have been sent to your Telegram Saved Messages")
         break
 
-    elif select == "p":
-        print("""\nPyrogram selected.\nRunning script...""")
-        time.sleep(1)
-        print(tutor)
-        with Client(
-        "UserBot", 
-        api_id=int(input("Enter API ID: ")),
-        api_hash=input("Enter API HASH: ")) as pyrogram:
-            saved_messages_template = "Pyrogram session" + template.format(pyrogram.export_session_string())
-            print("\nGenerating String session...\n")           
-            pyrogram.send_message("me", saved_messages_template, parse_mode="html")
-            time.sleep(1) 
-            print("Your STRING_SESSION value have been sent to your Telegram Saved Messages")
-        break
+    elif select == "n":
+         break
     
     else:
-        print("\nPlease only select P or T\n")
+        print("\nPlease only select y or n\n")
         time.sleep(1.5)
